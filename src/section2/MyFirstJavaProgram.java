e041ad817cb04040ab0eda547ba71b2893675618
gabreb <62906858+gabreb@users.noreply.github.com>
2020-03-31 14:57:58 -0700

    hi

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -6,9 +6,29 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
+		Robot rob = new Robot(); 
+		rob.setSpeed(999999); 
+		rob.penDown();
+		rob.setPenWidth(50);
+		rob.setPenColor(0, 0, 0);
+		rob.turn(-90);
+		rob.move(200);
+		rob.turn(-90);
+		rob.move(200);
+		rob.turn(-90);
+		rob.move(1);
+		rob.turn(-90);
+		rob.move(100);
 		
-		// START HERE
+		for (int i = 0; i > -1; i++) {
+			rob.move(300);
+			rob.turn(90); 
+			rob.move(25);
+			rob.turn(-90); 
+		}
 	
+	
+
 		
 		
 	}
