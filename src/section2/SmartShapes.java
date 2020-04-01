be67d56eb2fd896c8c1e6e87efc108c153cbd7cc
gabreb <62906858+gabreb@users.noreply.github.com>
2020-04-01 15:01:15 -0700

    hi

--- src/section2/SmartShapes.java
+++ src/section2/SmartShapes.java
@@ -3,27 +3,26 @@ package section2;
 import org.jointheleague.graphical.robot.Robot;
 
 public class SmartShapes {
-	
-    public static void main(String[] args) throws Exception {
-        // 1. Make a new Robot
 
+	public static void main(String[] args) throws Exception {
+		// 1. Make a new Robot
+		Robot rob = new Robot("batman");
 
-        // 3. Put the robot's pen down
+		// 3. Put the robot's pen down
+		rob.penDown();
 
+		// 6. Make the robot move as fast as possible
+		rob.setSpeed(100);
 
-        // 6. Make the robot move as fast as possible
+		// 5. Use a for loop to repeat everything below 4 times.
+		for (int i = 0; i < 10; i++) {
 
-
-        // 5. Use a for loop to repeat everything below 4 times. 
-
-
-        //         2. Move your robot 200 pixels
-
-
-        //         4. Turn the robot 90 degrees to the right
-
-    	
-    		// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides), pentagon (5-sides), decagon (10-sides)).
-   
-    }
+			// 2. Move your robot 200 pixels
+rob.move(140);
+			// 4. Turn the robot 90 degrees to the right
+rob.turn(36);
+			// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides),
+			// pentagon (5-sides), decagon (10-sides)).
+		}
+	}
 }
